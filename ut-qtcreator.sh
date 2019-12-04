947496416186ccc16d2c9ae2c142d770df45b65d
Lionel Duboeuf <lionel.duboeuf@actility.com>
2019-12-04 09:04:04 +0100

    fix wrong volume binding

--- ut-qtcreator.sh
+++ ut-qtcreator.sh
@@ -5,7 +5,7 @@
 
 IMAGE="ut-qtcreator:base"
 CONTAINER_NAME=ut-qtcreator
-FAKE_HOME=/home/lionel/dev/ubuntutouch
+FAKE_HOME=/home/dev/ubuntutouch
 
 
 xhost +
@@ -14,5 +14,5 @@ docker run --rm --net=host \
     -u `id -u`:`id -g` -e DISPLAY -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$FAKE_HOME:/home/$(id -un)" \
+    --volume="/home/$(id -un):$FAKE_HOME" \
 $IMAGE
